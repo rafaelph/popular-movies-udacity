b140c6f4052a02ece499be8bd75f9e2cd753c70b
Rafael Delos Santos <rkdelossantos@gmail.com>
2016-06-04 16:36:06 +1000

    Added grid view for poster thumbnails using sample images

--- app/src/main/java/com/rafael/popularmovies/MainDiscoveryScreenFragment.java
+++ app/src/main/java/com/rafael/popularmovies/MainDiscoveryScreenFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +18,11 @@ public class MainDiscoveryScreenFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_discovery_screen, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main_discovery_screen, container, false);
+
+        GridView gridView = (GridView)rootView.findViewById(R.id.gridView);
+        gridView.setAdapter(new MoviePosterAdapter(getActivity().getApplicationContext()));
+
+        return rootView;
     }
 }
---
+++ app/src/main/java/com/rafael/popularmovies/MoviePosterAdapter.java
@@ -0,0 +1,73 @@
+package com.rafael.popularmovies;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MoviePosterAdapter extends BaseAdapter {
+
+    private Context context;
+
+    private Integer[] sampleThumbnails = {
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_1, R.drawable.sample_2,
+            R.drawable.sample_3, R.drawable.sample_4
+    };
+
+    public MoviePosterAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return sampleThumbnails.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View currentView, ViewGroup viewGroupParent) {
+        ImageView imageView = buildViewToDisplay(currentView);
+        imageView.setImageResource(sampleThumbnails[position]);
+        return imageView;
+    }
+
+    @NonNull
+    private ImageView buildViewToDisplay(View currentView) {
+        ImageView imageView;
+        if (isViewRecycled(currentView)) {
+            imageView = buildViewFromScratch();
+        } else {
+            imageView = (ImageView) currentView;
+        }
+        return imageView;
+    }
+
+    @NonNull
+    private ImageView buildViewFromScratch() {
+        ImageView imageView = new ImageView(this.context);
+        imageView.setLayoutParams(new GridView.LayoutParams(480, 480));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setPadding(8, 8, 8, 8);
+        return imageView;
+    }
+
+    private boolean isViewRecycled(View currentView) {
+        return currentView == null;
+    }
+}
