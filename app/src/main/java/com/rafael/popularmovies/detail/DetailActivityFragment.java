ce547760aed4cd2bb22882d4c88e6977b4e46091
Rafael Delos Santos <rkdelossantos@gmail.com>
2016-06-13 10:54:07 +1000

    Included parsing of release date to include in movie detail page

--- app/src/main/java/com/rafael/popularmovies/detail/DetailActivityFragment.java
+++ app/src/main/java/com/rafael/popularmovies/detail/DetailActivityFragment.java
@@ -2,6 +2,7 @@ package com.rafael.popularmovies.detail;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +11,16 @@ import android.widget.TextView;
 
 import com.rafael.popularmovies.Movie;
 import com.rafael.popularmovies.R;
-import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import static com.rafael.popularmovies.Movie.MOVIE_EXTRA;
+import static com.squareup.picasso.Picasso.with;
+import static java.lang.String.valueOf;
 
 public class DetailActivityFragment extends Fragment {
 
@@ -25,17 +33,15 @@ public class DetailActivityFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         ImageView movieThumbnail = (ImageView) rootView.findViewById(R.id.movieThumbnailView);
+        FragmentActivity currentActivity = getActivity();
 
-        Bundle extrasFromIntent = getActivity().getIntent().getExtras();
+        Bundle extrasFromIntent = currentActivity.getIntent().getExtras();
         if (extrasFromIntent != null) {
             Movie movie = getMovieFromParcelable(extrasFromIntent);
-            TextView movieTitle = (TextView) rootView.findViewById(R.id.movieTitleTextView);
-            TextView description = (TextView) rootView.findViewById(R.id.synopsisText);
-            movieTitle.setText(movie.getMovieTitle());
-            description.setText(movie.getDescription());
-            Picasso.with(getActivity()).load(movie.getImageResource()).into(movieThumbnail);
+            populateViewsFromMovie(rootView, movie);
+            with(currentActivity).load(movie.getImageResource()).into(movieThumbnail);
         } else {
-            Picasso.with(getActivity()).load(R.mipmap.ic_launcher).into(movieThumbnail);
+            with(currentActivity).load(R.mipmap.ic_launcher).into(movieThumbnail);
         }
 
         return rootView;
@@ -43,5 +49,27 @@ public class DetailActivityFragment extends Fragment {
 
     private Movie getMovieFromParcelable(Bundle extrasFromIntent) {
         return extrasFromIntent.getParcelable(MOVIE_EXTRA);
+    }
+
+    private void populateViewsFromMovie(View rootView, Movie movie) {
+        TextView movieTitle = (TextView) rootView.findViewById(R.id.movieTitleTextView);
+        TextView description = (TextView) rootView.findViewById(R.id.synopsisText);
+        TextView releaseYearView = (TextView) rootView.findViewById(R.id.yearText);
+
+        movieTitle.setText(movie.getMovieTitle());
+        description.setText(movie.getDescription());
+        releaseYearView.setText(getReleaseYearFromMovie(movie));
+    }
+
+    private String getReleaseYearFromMovie(Movie movie) {
+        try {
+            Calendar calendar = Calendar.getInstance();
+            Date releaseDate = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(movie.getReleaseDate());
+            calendar.setTime(releaseDate);
+            return valueOf(calendar.get(Calendar.YEAR));
+
+        } catch (ParseException e) {
+            return "N/A";
+        }
     }
 }
